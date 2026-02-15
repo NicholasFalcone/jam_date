a5207ea5f8acb4d1414f34b407f9e86bea902cf5
Nicholas <nicholasfal19@gmail.com>
2026-02-15 23:11:41 +0100

    Added new crossair

--- source/Game/Crossair.lua
+++ source/Game/Crossair.lua
@@ -1,9 +1,13 @@
 class('Crossair').extends()
 
+local gfx = playdate.graphics
+
 function Crossair:init()
     self.x = 400 / 2
     self.y = 240 / 2
     self.hitRadius = 0  -- Default no radius, will be set by weapon type
+    self.shotgunReticle = gfx.image.new("Sprites/Crossair_shotgun")
+    self.normalReticle = gfx.image.new("Sprites/Crossair")
 end
 
 function Crossair:move(x, y)
@@ -14,12 +18,16 @@ end
 function Crossair:draw()
     local gfx = playdate.graphics
 
-    gfx.setColor(gfx.kColorBlack)
-    gfx.drawLine(self.x - 10, self.y, self.x + 10, self.y)
-    gfx.drawLine(self.x, self.y - 10, self.x, self.y + 10)
-    
-    -- Draw hit radius circle if > 0
-    if self.hitRadius and self.hitRadius > 0 then
-        gfx.drawCircleAtPoint(self.x, self.y, self.hitRadius)
+    -- Choose reticle image based on hitRadius: shotgun if radius > 0, otherwise normal
+    local reticle = (self.hitRadius and self.hitRadius > 0) and self.shotgunReticle or self.normalReticle
+    if reticle then
+        local rw, rh = reticle:getSize()
+        local dx = math.floor(self.x - (rw / 2))
+        local dy = math.floor(self.y - (rh / 2))
+        reticle:draw(dx, dy)
     end
+
+    -- Draw small central dot (pallino)
+    gfx.setColor(gfx.kColorBlack)
+    gfx.fillCircleAtPoint(self.x, self.y, 2)
 end
