f763334b6cff86b05a39f74e06629bbea46c3db9
Nicholas <nicholasfal@live.it>
2026-02-16 22:51:11 +0100

    Check timer

--- source/Core/GameManager.lua
+++ source/Core/GameManager.lua
@@ -99,7 +99,6 @@ function GameManager:update(deltaTime)
 		if self.mainMusic then
 			local nextRate = 1 + (self.timeAlive * 0.0083) -- Gradually increase pitch over time (max 2x at 2 minutes)
 			nextRate = clamp(nextRate, 1, 1.5) -- Cap the pitch increase at 1.5x for better audio quality
-			print("Time Alive: " .. string.format("%.2f", self.timeAlive) .. "s, Setting music rate to: " .. string.format("%.2f", nextRate))
 			self.mainMusic:setRate(nextRate) -- Gradually increase pitch over time (max 2x at 2 minutes)
 		end
 	end
